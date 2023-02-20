07a4d62a9aad9696244cd294102a9da5a9ec3dc4
Justin Williams <williams.jman@gmail.com>
2023-02-20 03:00:13 -0500

    Added the basic 8 tasks

---
+++ src/components/TaskList.js
@@ -0,0 +1,39 @@
+function TaskList() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      item: "Daily Standup",
+    },
+    {
+      id: 2,
+      item: "Code review the last project",
+    },
+    {
+      id: 3,
+      item: "Do research about how React works",
+    },
+    {
+      id: 4,
+      item: "Do research about how React works",
+    },
+    {
+      id: 5,
+      item: "Do research about how React works",
+    },
+    {
+      id: 6,
+      item: "Do research about how React works",
+    },
+    {
+      id: 7,
+      item: "Do research about how React works",
+    },
+    {
+      id: 8,
+      item: "Do research about how React works",
+    },
+  ]);
+
+  return <div></div>;
+}
+export default TaskList;
